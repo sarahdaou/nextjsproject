2ef3360e50e128d3733f55c56a3b8012d2331116
Sarah Daou <adaou.sarah@gmail.com>
2024-08-19 22:45:58 +0300

    footer done, still have banner

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,16 +1,23 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <footer className="footer">
+    <footer className="footer">
         <div className="footer-content container">
             <div className="layout">
                 <p>2024 All rights reserved.</p>
             </div>
-            <div className="footer-icons icon-link">
-                <Image src="/instagram-icon.png" alt="Instagram" height={30} width={30} />
-                <Image src="/whatsapp.png" alt="WhatsApp" height={30} width={0} style={{ width:'auto'}}/>
-                <Image src="/youtube.png" alt="YouTube" height={30} width={0} style={{ width:'auto'}}/>
+            <div className="footer-icons">
+                <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="pics">
+                    <Image src="/instagram-icon.png" alt="Instagram" height={30} width={30}  />
+                </Link>
+                <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="pics">
+                    <Image src="/facebook-icon.png" alt="Facebook" height={30} width={30}  />
+                </Link>
+                <Link href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="pics">
+                    <Image src="/twitter-icon.png" alt="Twitter" height={30} width={30}  />
+                </Link>
             </div>
         </div>
     </footer>
